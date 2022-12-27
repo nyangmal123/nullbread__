c9c2627a8bf1233de9fb4331bf8ff60ed5f1ecff
go <sukpo6010@gmail.com>
2022-12-27 20:22:21 +0900

    서버오류수정2

--- src/redux/modules/constant.js
+++ src/redux/modules/constant.js
@@ -1,4 +1,4 @@
-export const AXIOS_ADDRESS = "http://localhost:3003";
-// const AXIOS_ADDRESS = 'https://cool-rigorous-umbra.glitch.me/';
+// export const AXIOS_ADDRESS = "http://localhost:3003";
+const AXIOS_ADDRESS = "https://cool-rigorous-umbra.glitch.me/";
 
 export default AXIOS_ADDRESS;
