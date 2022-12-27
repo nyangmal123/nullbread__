2126b99a4875cbad350017db37ac19470da93560
MarinaNam <namma123@naver.com>
2022-12-28 00:11:42 +0900

    배포 3차 수정, __deletPost수정

--- src/redux/modules/constant.js
+++ src/redux/modules/constant.js
@@ -1,4 +1,5 @@
 // export const AXIOS_ADDRESS = "http://localhost:3003";
-const AXIOS_ADDRESS = "https://cool-rigorous-umbra.glitch.me/";
+
+const AXIOS_ADDRESS = 'https://cool-rigorous-umbra.glitch.me/';
 
 export default AXIOS_ADDRESS;
