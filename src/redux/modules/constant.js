4dbe98897c00fc5be75691b15b49cf7279606d7f
MarinaNam <namma123@naver.com>
2022-12-27 19:22:35 +0900

    glitch 배포

--- src/redux/modules/constant.js
+++ src/redux/modules/constant.js
@@ -1,4 +1,4 @@
 // export const AXIOS_ADDRESS = 'http://localhost:3003';
-const AXIOS_ADDRESS = 'nullbread-server-fdlzg7ivx-nyangmal123.vercel.app';
+const AXIOS_ADDRESS = 'https://cool-rigorous-umbra.glitch.me/';
 
 export default AXIOS_ADDRESS;
